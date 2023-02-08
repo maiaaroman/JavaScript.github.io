23d539fd2e5e7d0e04067b330c03c0c481efbb70
maiaaroman <118064071+maiaaroman@users.noreply.github.com>
2023-02-07 21:59:23 -0300

    Add files via upload
    cambio de codigo

--- main.js
+++ main.js
@@ -1,51 +1,36 @@
 alert("Bienvenido/a a la calculadora de gastos");
 
-const nombreCompleto = "Maia Roman";
-const contraseniaAutorizada = "2023";
+const nombreCompleto = prompt("Ingrese su nombre: ");
+const passwordAutorizado = prompt("Cree una contraseña: ");
 
-for(let i = 0; i = 1; i++) {
-    let nombre = prompt("Ingrese su nombre completo: ");
-    let contrasenia = prompt("Ingrese su contraseña: ");
+let nombre = prompt("Ingrese su nombre completo: ");
+let password = prompt("Ingrese la contraseña creada: ");
 
-    if(nombre === nombreCompleto && contraseniaAutorizada === contrasenia) {
-    alert("Inicio de sesion autorizado " + nombreCompleto);
-    break;
-    } else {
-    alert("Nombre o Contraseña incorrecto/a, intente nuevamente");
+while(nombreCompleto != nombre || passwordAutorizado != password) {
+    alert("Nombre o contraseña incorrecta, ingrese nuevamente")
+    nombre = prompt("Ingrese su nombre completo: ");
+    contrasenia = prompt("Ingrese la contraseña creada: ");
 }
-
-do {
-    contrasenia = prompt("Ingrese su contraseña: ");
-} while(contrasenia != contraseniaAutorizada);
-}
+alert("Ingreso correctamente")
 
 let cantidadGastos = parseInt(prompt("Ingrese la cantidad de gastos que tuvo este mes: "));
+let suma = 0;
 
-for(let i = 0; i < 1; i++) {
-    let primerMonto = parseInt(prompt("Ingrese primer monto: "));
-    let segundoMonto = parseInt(prompt("Ingrese segundo monto: "));
-    let tercerMonto = parseInt(prompt("Ingrese tercer monto: "));
-    let total = calcular(primerMonto, segundoMonto, tercerMonto);
-    alert("El gasto total de este mes fue de $" + total.toFixed(2));
+for(let i = 0; i < cantidadGastos; i++) {
+    
+    let n = i + 1
+    let monto = parseInt(prompt("Ingrese el monto numero " + n))
+    suma = suma + monto
+    console.log(suma)
 }
+alert("El total gastado este mes fue de $" + suma)
 
-function calcular(primerMonto, segundoMonto, tercerMonto) {
-    let total = (primerMonto + segundoMonto + tercerMonto);
-    return total;
-}
-
-let gasto = buenGasto(total);
-
-if(buenGasto) {
-    alert(nombreCompleto + " estuvo bien este mes");
-} else {
-    alert(nombreCompleto + " te pasaste un poco este mes!");
-}
-
-function buenGasto(total) {
-    if(total <= 50000) {
-        return true;
+function buenGasto(Total) {
+    if(Total <= 50000) {
+        alert("Buen gasto este mes")
     } else {
-        return false;
+        alert("Gastaste mucho este mes")
     }
 }
+
+buenGasto(suma)
